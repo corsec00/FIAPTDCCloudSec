b4ad1bd8ee4e1be1f9e1270a6e3e87cdf71df1de
Leo Santos <corsec00@gmail.com>
2024-09-08 02:25:29 -0300

    Update cert-CA.ps1

--- cert-CA.ps1
+++ cert-CA.ps1
@@ -1,11 +1,19 @@
-cls
+#####################################################
+### Criado por Leonardo Santos Silva
+### Data: 15 de Setembro de 2023
+### Última Revisão: 8 de Setembro de 2024
+### Exercício: 05 - Gerenciamento de ferramenta de criptografia (Key Vault com  Bastion & VPN) - FIAP 2TDCPR-2024
+#####################################################
+
+Clear-Host
+
 # Definição das Variáveis
 $RootCA = Read-Host "Digite o Nome do certificado ROOT da sua CA"  
 $CertName = Read-Host "Digite o Nome do certificado do Cliente ou o seu nome"  
 $Senha = Read-Host "Digite uma senha para exportar o certificado ROOT da sua CA para a Base64" -AsSecureString
 $Senhabase64 = ConvertTo-SecureString -String $Senha -Force -AsPlainText
 
-# Certificado ROOT da CA
+# Criando Certificado ROOT da CA
 $params = @{
     Type = 'Custom'
     Subject = $RootCA
@@ -21,11 +29,11 @@ $params = @{
 $certROOT = New-SelfSignedCertificate @params
 $certROOT
 
-#Exportando as configurações do ROOT CA
+# Exportando as configurações do ROOT CA
 $myThumbprint = Get-ChildItem -Path "Cert:\CurrentUser\My" | Where-Object { $_.Subject -like "*$RootCA*" } | Select-Object -ExpandProperty Thumbprint
 $certThumb = Get-ChildItem -Path "Cert:\CurrentUser\My\$myThumbprint"
 
-#Cria o Certificado do Client
+#Criando o Certificado do Cliente
 $params = @{
     Type = 'Custom'
     Subject = $CertName
